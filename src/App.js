1c36451088f40049cd77d5ef18edf0ecbc0e6f03
James Jones <jjsfitnesstoronto@gmail.com>
2025-11-12 03:08:15 -0500

    Completed Wk 11 lab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UserForm from './components/UserForm';
+import UserSignupForm from './components/UserSignupForm';
 
 function App() {
   return (
     <div className="App">
       <h1>React Form Examples</h1>
-      <UserForm />
+      {/*<UserForm />*/}
+      <UserSignupForm/>
     </div>
   );
 }
