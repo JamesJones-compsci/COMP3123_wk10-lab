ce8ce2833e002e745bb1b9658aadf6b9d7d0d615
James Jones <jjsfitnesstoronto@gmail.com>
2025-11-12 02:41:49 -0500

    Adding a User Sign up Form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -9,6 +9,8 @@ const INITIAL_USER_DATA = {
 export default function UserForm() {
   const [userData, setUserData] = useState(INITIAL_USER_DATA);
 
+  const [errorObj, setErrorsObj] = useState({});
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUserData({
@@ -19,9 +21,22 @@ export default function UserForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(validateForm()){
     console.log('User Data Submitted:', userData);
+    // console.log("Errors: ", errorObj);  This is not required
+    // Make the API call here
     setUserData(INITIAL_USER_DATA);
+    }
   };
+
+  const validateForm = () => {
+    setErrorsObj({});
+    if(userData.name.trim() === '') setErrorsObj(prev => ({...prev, name: 'Name is required'}));
+    if(userData.email.trim() === '') setErrorsObj(prev => ({...prev, email: 'Email is required'}));
+    if(userData.password.trim() === '') setErrorsObj(prev => ({...prev, password: 'Password is required'}));
+
+    return Object.keys(errorObj).length === 0;
+  }
 
   return (
     <div>
@@ -36,6 +51,7 @@ export default function UserForm() {
             onChange={handleChange}
           />
         </label>
+        {errorObj.name && <span style={{color: 'red'}}> - {errorObj.name}</span>}
         <br />
         <label>
           Email:
@@ -46,6 +62,7 @@ export default function UserForm() {
             onChange={handleChange}
           />
         </label>
+        {errorObj.email && <span style={{color: 'red'}}> - {errorObj.email}</span>}
         <br />
         <label>
           Password:
@@ -56,6 +73,7 @@ export default function UserForm() {
             onChange={handleChange}
           />
         </label>
+        {errorObj.password && <span style={{color: 'red'}}> - {errorObj.password}</span>}
         <br />
         <button type="submit">Submit</button>
       </form>
